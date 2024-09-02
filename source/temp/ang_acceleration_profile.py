24b4ebf7acf74829ca709c0c76685454f319a3f7
Max Greiner <max.greiner.ch@gmail.com>
2024-09-02 15:56:10 +0200

    Some changes and experimentation. Adjusted alpha(t)-function for correct angular velocity profile. Problems; cannot have tail swing freely. No clue on how to measure torque.

--- source/temp/ang_acceleration_profile.py
+++ source/temp/ang_acceleration_profile.py
@@ -24,34 +24,100 @@ import torch
 import os
 
 import omni.isaac.core.utils.prims as prim_utils
+from omni.isaac.core.articulations import ArticulationView
 
 import omni.isaac.lab.sim as sim_utils
-from omni.isaac.lab.assets import Articulation, ArticulationCfg, RigidObjectCfg, RigidObject
+from omni.isaac.lab.assets import Articulation, ArticulationCfg, ArticulationData, RigidObjectCfg, RigidObject
 from omni.isaac.lab.sim import SimulationContext
 from omni.isaac.lab.actuators import ImplicitActuatorCfg, ActuatorBaseCfg
+import omni.physx as physx
 
 ### Ang Acceleration Profile ###
 import numpy as np
 class SimpleAngAccelProfile:
-    def __init__(self, sim_dt: float, acceleration: float = 200.0, acceleration_dt: float = 0.3, 
-                 start_acceleration_time: float = 0.0, stop_time: float = 0.8):
+    def __init__(self, 
+                 sim_dt: float, 
+                 a: float = 200.0, 
+                 t0: float = 1.5,
+                 t0_t1: float = 0.5,
+                 t1_t2: float = 0.2):
+        """Simple angular acceleration profile defined as follows;
+        alpha(t) = a*(t-t0), for t0 < t < t1
+        alpha(t) = a*(t1-t0), for t1 < t < t2
+        alpha(t) = 0, otherwise
+        
+        All variables t are in seconds.
+        
+        Args:
+        - sim_dt: Simulation time-discretization in seconds.
+        - a: Angular acceleration in rad/s^2.
+        - t0: Start time for acceleration in seconds.
+        - t0_t1: Time duration for acceleration in seconds (mathematically: t1-t0).
+        - t1_t2: Time for constant angular velocity in seconds (mathematically: t2-t1).
+        
+        """
         self.sim_dt = sim_dt
-        self.acceleration = acceleration
-        self.acceleration_dt = acceleration_dt
-        self.start_acceleration_time = start_acceleration_time
-        self.stop_time = stop_time
-
-
-    def get_ang_vel(self, dt: int) -> float:
-        "Returns angular velocity in rad/s at simulation setp dt (aka 'count')."
-        if dt*self.sim_dt < self.start_acceleration_time:
-            return 0.0
-        elif dt*self.sim_dt < self.acceleration_dt:
-            return self.acceleration * dt * self.sim_dt
-        elif dt*self.sim_dt < self.stop_time:
-            return self.acceleration * self.acceleration_dt
+        self.acceleration = a
+        self.t0 = t0
+        self.t1 = t0 + t0_t1
+        self.t2 = self.t1 + t1_t2
+    
+    def get_ang_vel(self, count: int):
+        "Returns angular velocity in rad/s at simulation step count."
+        current_time = count * self.sim_dt
+        if current_time < self.t0:
+            return None
+        elif current_time < self.t1:
+            return self.acceleration * (current_time - self.t0)
+        elif current_time < self.t2:
+            return self.acceleration * (self.t1 - self.t0)
         else:
-            return 0.0
+            return None
+
+### CFG ###
+BOX_CFG = ArticulationCfg(
+    prim_path="/World/Origin.*/Robot",
+    spawn=sim_utils.UsdFileCfg(
+        usd_path="source/temp/box_w_tail.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            rigid_body_enabled=True,
+            # max_linear_velocity=1000.0,
+            # max_angular_velocity=1000.0,
+            # max_depenetration_velocity=100.0,
+            enable_gyroscopic_forces=True,
+            # kinematic_enabled=True
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            articulation_enabled=True,
+            enabled_self_collisions=True,
+            fix_root_link=True
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0, 0, 0.5), 
+        joint_pos={"box_to_rod": -1.5708}, 
+        # joint_pos={"box_to_rod": -0.3},
+        # joint_vel={"box_to_rod": 10.0}
+    ),
+    actuators={
+        # "rod_torque": ActuatorBaseCfg(
+        #     joint_names_expr=["box_to_rod"],
+        #     friction=1.0,
+        #     effort_limit=1000.0,
+        #     velocity_limit=100.0,
+        #     stiffness=0.0,
+        #     damping=10.0,
+        #     ),
+        "rod_motor" : ImplicitActuatorCfg(
+            joint_names_expr=["box_to_rod"],
+            friction=1.0,
+            damping=10.0,
+            effort_limit=1000.0,
+            stiffness=0.0,
+            velocity_limit=500.0
+        ),
+        },  
+)
 
 
 ### FUNCTIONS ###
@@ -71,52 +137,9 @@ def design_scene() -> tuple[dict, list[list[float]]]:
     origins = [[0, 0, z], [2, 0, z], [4, 0, z], 
                [0, 2, z], [2, 2, z], [4, 2, z]]
     for i, origin in enumerate(origins):
-        prim_utils.create_prim(f"/World/Origin{i+1}", "Xform", translation=origin)
+        prim_utils.create_prim(f"/World/Origin{i+1}", "Xform", translation=origin)    
 
-    box_cfg = ArticulationCfg(
-        prim_path="/World/Origin.*/Robot",
-        spawn=sim_utils.UsdFileCfg(
-            usd_path="source/temp/box_w_tail.usd",
-            rigid_props=sim_utils.RigidBodyPropertiesCfg(
-                rigid_body_enabled=True,
-                # max_linear_velocity=1000.0,
-                # max_angular_velocity=1000.0,
-                # max_depenetration_velocity=100.0,
-                enable_gyroscopic_forces=True,
-                # kinematic_enabled=True
-            ),
-            articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-                articulation_enabled=True,
-                enabled_self_collisions=True,
-                fix_root_link=True
-            ),
-        ),
-        init_state=ArticulationCfg.InitialStateCfg(
-            pos=(0, 0, 0.5), 
-            joint_pos={"box_to_rod": -1.5708}, 
-            # joint_pos={"box_to_rod": -0.3},
-            # joint_vel={"box_to_rod": 10.0}
-        ),
-        actuators={
-            # "rod_motor": ActuatorBaseCfg(
-            #     joint_names_expr=["box_to_rod"],
-            #     effort_limit=400.0,
-            #     velocity_limit=100.0,
-            #     stiffness=0.0,
-            #     damping=10.0,
-            #     ),
-            "rod_motor" : ImplicitActuatorCfg(
-                joint_names_expr=["box_to_rod"],
-                damping=10.0,
-                effort_limit=400.0,
-                stiffness=0.0,
-                velocity_limit=100.0
-            )
-            },
-            
-    )
-
-    box = Articulation(cfg=box_cfg)
+    box = Articulation(cfg=BOX_CFG)
 
     # return the scene information
     scene_entities = {"box": box}
@@ -126,6 +149,12 @@ def design_scene() -> tuple[dict, list[list[float]]]:
 def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, Articulation], origins: torch.Tensor):
     "Runs the simulator."
     robot = entities["box"]
+
+    # Initialize ArticulationData using physx.ArticulationView
+    articulation_view = ArticulationView(prim_paths_expr="/World/Origin.*/Robot")
+    articulation_view.initialize()
+
+    # d = ArticulationData(robot.root_physx_view(), sim.device)
 
     # Sim step
     sim_dt = sim.get_physics_dt()
@@ -177,16 +206,31 @@ def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, Articula
         # robot.set_joint_effort_target(efforts)
 
         # Follow ang-vel profile
-        ang_vel = ang_vel_profile.get_ang_vel(dt=count)
-        joint_vel = torch.full_like(robot.actuators['rod_motor'].applied_effort, ang_vel)
-        robot.set_joint_velocity_target(joint_vel)
+        ang_vel = ang_vel_profile.get_ang_vel(count=count)
+        # print(f"[C: {count}]: Ang-vel: {ang_vel}")
+        
+        if ang_vel is not None:
+            joint_vel = torch.full_like(robot.actuators['rod_motor'].applied_effort, ang_vel)
+            robot.set_joint_velocity_target(joint_vel)
+        else:
+            # TODO: How can I have tail swing freely?
+            robot.set_joint_velocity_target(torch.zeros_like(robot.actuators['rod_motor'].applied_effort))
+        
+        print(f"[C: {count}]: Ang-vel setpoint: {ang_vel}, Target_joint_vel: {robot._joint_vel_target_sim[0]}")
 
-        robot.write_data_to_sim()
+        robot.write_data_to_sim()        
 
-        if count % 50 == 0:
-            # print(f"Current effort at {base_effort} with count = {count}")
-            # print(f"Robot joint efforts: {robot._joint_effort_target_sim}")
-            print(f"Current ang-vel at {ang_vel} with count = {count}")
+        measured_joint_efforts = articulation_view.get_measured_joint_efforts()
+
+        # if count % 5 == 0:
+        #     print(f"[C: {count}]: Ang-vel: {ang_vel}, Effort: {measured_joint_efforts[0].float()}")
+        #     print(f"    Vel-Target: {robot._joint_vel_target_sim}")
+        # if count % 50 == 0:
+        #     # print(f"Current effort at {base_effort} with count = {count}")
+        #     # print(f"Robot joint efforts: {robot._joint_effort_target_sim}")
+        #     print(f"Current ang-vel at {ang_vel} with count = {count}")
+        #     print(f"Robot_joint_efforts: {measured_joint_efforts}")
+            
 
         sim.step() 
         count += 1
